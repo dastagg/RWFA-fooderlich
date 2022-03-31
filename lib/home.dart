446cff1d9ab4dfff0b3ec3dff4c788d3c9c2af3d
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-31 15:48:04 -0400

    Add home.dart, update main.dart to use it.
    Also:
    Add BottomNavigationBar and update the Container with specific color.

---
+++ lib/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+// 1
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    // TODO: Replace with Cart1
+    Container(color: Colors.red),
+    // TODO: Replace with Cart2
+    Container(color: Colors.green),
+    // TODO: Replace with Cart3
+    Container(color: Colors.blue),
+  ];
+
+  // 9
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          // 2
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // 5
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        // 11
+        onTap: _onItemTapped,
+        // 6
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
