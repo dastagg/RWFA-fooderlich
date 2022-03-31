820707f8fdab8234d6f1447b1dcb45a36721280f
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-31 15:31:24 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# fooderlich
+
+A Flutter Apprentice tutorial by Ray Wenderlich. 
+
+## Description
+This is Section II: Chapter 3 of the Flutter Apprentice Tutorial titled:
+
+### 3. Basic Widgets
+
+Use different types of widgets to build the foundation of a new app.
+
+Three types of cards will be created: the main recipe card, the author card, and an explore card.
+
+This tutorial will also introduce "standard Theme class" to create a default app style.
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        body: Center(
+          child: Text(
+            'Let\'s get cooking 👩‍🍳',
+            style: theme.textTheme.headline1,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
